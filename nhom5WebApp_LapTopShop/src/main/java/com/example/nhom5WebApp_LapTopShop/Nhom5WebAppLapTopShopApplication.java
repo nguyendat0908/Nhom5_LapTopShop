6e3cd66bed527b04af3019c04778db9dd5e4e2e7
Nguyendat0908 <Nguyendat090803@gmail.com>
2024-08-26 16:00:17 +0700

    First Commit

---
+++ nhom5WebApp_LapTopShop/src/main/java/com/example/nhom5WebApp_LapTopShop/Nhom5WebAppLapTopShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.nhom5WebApp_LapTopShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nhom5WebAppLapTopShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nhom5WebAppLapTopShopApplication.class, args);
+	}
+
+}
---
+++ nhom5WebApp_LapTopShop/src/test/java/com/example/nhom5WebApp_LapTopShop/Nhom5WebAppLapTopShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nhom5WebApp_LapTopShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nhom5WebAppLapTopShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
