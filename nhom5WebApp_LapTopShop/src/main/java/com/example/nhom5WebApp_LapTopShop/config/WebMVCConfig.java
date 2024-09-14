a152d3fcec2efedb71d2e25d408a3d13e4cc3969
Nguyendat0908 <Nguyendat090803@gmail.com>
2024-09-14 14:08:54 +0700

    Config MVC

---
+++ nhom5WebApp_LapTopShop/src/main/java/com/example/nhom5WebApp_LapTopShop/config/WebMVCConfig.java
@@ -0,0 +1,42 @@
+package com.example.nhom5webapp_laptopshop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+public class WebMVCConfig implements WebMvcConfigurer {
+
+    // Tìm view để trả về
+    @Bean
+    public ViewResolver viewResolver() {
+        final InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setViewClass(JstlView.class);
+        bean.setPrefix("/WEB-INF/view");
+        bean.setSuffix(".jsp");
+
+        return bean;
+    }
+
+    // Đăng ký view để xử lý ứng dụng
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.viewResolver(viewResolver());
+    }
+
+    // Đăng ký tìm và giải quyết các file tĩnh
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("/resources/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("/resources/js/");
+        registry.addResourceHandler("/images/**").addResourceLocations("/resources/images/");
+        registry.addResourceHandler("/client/**").addResourceLocations("/resources/client/");
+    }
+}
