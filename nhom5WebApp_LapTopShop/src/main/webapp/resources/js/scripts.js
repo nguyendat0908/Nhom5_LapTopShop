121bf6ae55bcfd22adea92270c4b2e5f7622a47c
VanHoang0612 <nguyenvanhoang031206@gmail.com>
2024-10-02 13:07:16 +0700

    Validate product/create

---
+++ nhom5WebApp_LapTopShop/src/main/webapp/resources/js/scripts.js
@@ -0,0 +1,17 @@
+document
+  .getElementById("formFileSm")
+  .addEventListener("change", function (event) {
+    const file = event.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = function (e) {
+        const imgElement = document.getElementById("img-thumbnail");
+        imgElement.src = e.target.result;
+        imgElement.style.display = "block";
+      };
+
+      reader.readAsDataURL(file);
+    }
+  });
